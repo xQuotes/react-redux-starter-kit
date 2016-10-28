7769155e38cac65dde590c4e9a8ebf9604fc6744
fooying <fooying@qq.com>
2016-10-28 17:11:03 +0800

    page limit change

--- src/route/components/table.js
+++ src/route/components/table.js
@@ -12,9 +12,21 @@ export default class DataTable extends React.Component {
   }
   render() {
     const {columns, dataSource} = this.props
+    const pagination = {
+      total: dataSource.length,
+      showSizeChanger: true,
+      onShowSizeChange(current, pageSize) {
+        console.log('Current: ', current, '; PageSize: ', pageSize);
+      },
+      onChange(current) {
+        console.log('Current: ', current);
+      },
+    }
+
     return(
       <Table columns={columns}
         dataSource={dataSource}
+        pagination={pagination}
         scroll={{ x: 1000 }}/>
       )
   }
