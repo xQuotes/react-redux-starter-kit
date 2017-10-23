cd58ca1c555844e333e8163214b52bf1f161ad8e
fooying <fooying@qq.com>
2017-10-23 22:13:16 +0800

    portal

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import './style/main.less'
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
-registerServiceWorker()
+// registerServiceWorker()
