c68bd49f745b4d5fdc5cb720be5ec204404a33b2
fuyin [付引] <fuyin@xiaomi.com>
2018-01-22 10:13:37 +0800

    JSON

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -4,7 +4,8 @@ import Fetch from 'Fetch'
 import Api from 'Api'
 import Auth from 'Auth'
 import { errorAlert } from 'Utils'
-import queryString from 'query-string'
+// import queryString from 'query-string'
+const queryString = JSON
 import 'Arr'
 
 export default class Store {
