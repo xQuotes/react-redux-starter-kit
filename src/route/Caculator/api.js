2d2baebfb2280069da16c52f9efbdd5fa245235f
fuyin [付引] <fuyin@xiaomi.com>
2018-01-29 21:52:47 +0800

    计算器链接

--- src/route/Caculator/api.js
+++ src/route/Caculator/api.js
@@ -1,15 +1,16 @@
 export default {
   101: '/api/supervise/projectmanagement',
-  201: '/api/supervise/projectmanagement',
+  201: '/api/supervise/projectdesign',
   301: '/api/supervise/projectcost',
   401: '/api/supervise/projectmanagement',
-  501: '/api/supervise/projectmanagement',
+  501: '/api/supervise/projectbidding',
   601: '/api/supervise/projectmanagement',
   701: '/api/supervise/projectbefore',
   801: '/api/supervise/projectmanagement',
   901: '/api/supervise/projectmanagement',
   110: '/api/supervise/projectmanagement',
-  120: '/api/supervise/projectmanagement',
+  112: '/api/supervise/projectinvestigate',
+  120: '/supervise/projectconservation',
   // 造价计算器
   9: '/api/cost/getprojectcost'
 }
