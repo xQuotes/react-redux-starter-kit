7e3823af84076a553d1068cd3497673b9cac26cf
fuyin [付引] <fuyin@xiaomi.com>
2018-03-03 00:27:48 +0800

    dashborad

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd build
+git init 
+git remote add origin git@github.com:xQuotes/react-redux-starter-kit.git
+git add .
+git commit -m "deploy"
+git checkout -b portal-deploy
+git push origin HEAD
+
+
+
