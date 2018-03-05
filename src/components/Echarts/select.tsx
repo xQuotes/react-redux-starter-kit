4efec982fc8636fc7a93fae57a52050df1b8048f
fuyin [付引] <fuyin@xiaomi.com>
2018-03-05 09:50:24 +0800

    daima

---
+++ src/components/Echarts/select.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import 'echarts/map/js/china.js'
+import { Select } from 'antd'
+const Option = Select.Option
+
+import mapData from './const'
+
+export default class SelectComponent extends React.Component<any, {}> {
+  onChartClick = (param: any) => {
+    console.log(param)
+    const { store, typeName } = this.props // componentType = 'guide'
+    const data = JSON.parse(param)
+    if (typeName) {
+      store.setSelectMapItem(data, typeName, store.componentType)
+      return
+    }
+    if (store.itemType) {
+      store.setSelectMapItem(data, store.itemType)
+      // alert('该计算器是全国范围的')
+    } else {
+      store.setSelectMapItem(data, store.itemType)
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Select
+          size={'large'}
+          defaultValue={JSON.stringify({ name: '北京', value: 1 })}
+          style={{
+            width: 80,
+            margin: 10
+          }}
+          onChange={this.onChartClick}
+        >
+          {mapData.map((v, k) => {
+            return (
+              <Option
+                value={JSON.stringify({
+                  name: v.name,
+                  value: k
+                })}
+                key={k}
+              >
+                {v.name}
+              </Option>
+            )
+          })}
+        </Select>
+      </div>
+    )
+  }
+}
--- src/route/Index/standard/index.tsx
+++ src/route/Index/standard/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { inject, observer } from 'mobx-react'
 import { Row, Col, Button } from 'antd'
-import Echarts from '../../../components/Echarts/'
+import AreaSelect from '../../../components/Echarts/select'
 import { Link } from 'react-router-dom'
 
 import './standard.less'
@@ -37,43 +37,57 @@ export default class Standard extends React.Component<any, any> {
     return (
       <Row className="main-standard">
         <Col span={24} className="main-title">
-          计算规范
+          <div className="main-title-ch">
+            <span className="main-title-line"> &nbsp;</span>
+            <span>计算规范</span>
+            <span className="main-title-line"> &nbsp;</span>
+          </div>
+          <div className="main-title-en">Computation specification</div>
         </Col>
-        <Col span={15}>
-          <Echarts
-            style={{
-              width: '550px',
-              height: '550px'
-            }}
-            componentType={standardStore.componentType}
-            typeName={standardStore.typeName}
-            store={standardStore}
-          />
-        </Col>
-        <Col span={9} className="main-content">
-          <h3>{standardStore.selectMapItem.name}造价规范</h3>
-          <Button.Group>
-            <Button type="primary">定额</Button>
-            <Button className="default">清单</Button>
-          </Button.Group>
-          <article>
-            <Row className="main-content-row">
-              {list.map((val: any, key: any) => {
-                return (
-                  <Link
-                    key={key}
-                    to={{
-                      pathname: '/guide',
-                      search: `?type=${standardStore.typeName}&id=${val.id}`,
-                      state: { type: standardStore.typeName, id: val.id }
-                    }}
-                  >
-                    <Col className="main-content-col">{val.fileName}</Col>
-                  </Link>
-                )
-              })}
-            </Row>
-          </article>
+        <Col span={24}>
+          <Col span={11} className="guide-left">
+            <img src={require('../../../common/index/规范.jpg')} alt="" />
+
+            <Button.Group>
+              <Button type="primary">定额</Button>
+              <Button className="default">清单</Button>
+            </Button.Group>
+          </Col>
+          <Col span={11} offset={2} className="guide-right">
+            <div className="guide-header">
+              <div className="en-title">Sinking</div>
+              <div className="zh-title">造价规范</div>
+              <span className="br" />
+              <div className="area-select">
+                <AreaSelect
+                  componentType={standardStore.componentType}
+                  typeName={standardStore.typeName}
+                  store={standardStore}
+                />
+              </div>
+            </div>
+            <div className="guide-content">
+              <Row className="main-content-row">
+                {list.map((val: any, key: any) => {
+                  return (
+                    <Link
+                      key={key}
+                      to={{
+                        pathname: '/guide',
+                        search: `?type=${standardStore.typeName}&id=${val.id}`,
+                        state: { type: standardStore.typeName, id: val.id }
+                      }}
+                    >
+                      <Col className="main-content-col">{val.fileName}</Col>
+                    </Link>
+                  )
+                })}
+              </Row>
+            </div>
+            <div className="guide-footer">
+              <Button>查看更多</Button>
+            </div>
+          </Col>
         </Col>
       </Row>
     )
